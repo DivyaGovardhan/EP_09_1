72420cd27225a67198591e366e50ad3e9d077132
DivyaGovardhan <aslikyan2006@gmail.com>
2025-03-10 18:15:36 +0700

    task4.1

--- js/main.js
+++ js/main.js
@@ -8,5 +8,16 @@ let app = new Vue({
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inventory: 10,
         onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
     }
 })
